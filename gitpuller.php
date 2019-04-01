023a9ff237c1b3a5855e184976ebfcf13e603fe8
Martin Schilliger <martin_schilliger@bluewin.ch>
2019-04-01 21:18:44 +0200

    gitpuller funktioniert :-)

--- gitpuller.php
+++ gitpuller.php
@@ -1,9 +1,7 @@
 <?php
-
+// wird von GitHub als Webhook bei jeden Push zum Repository aufgerufen
 if ($_GET["token"] = "PRNuxt8F3X5Kd45qxzTpJBDanYdWXSSccBbXD4NQ") {
   exec("git pull");
 }
-
-echo "gugus";
 
 ?>
