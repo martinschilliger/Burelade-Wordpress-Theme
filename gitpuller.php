9b1711e4a997776c8d5f1ef76596e6707df30ddd
Martin Schilliger <martin.schilliger@rettung-sg.ch>
2020-11-02 13:06:09 +0100

    Push…

--- gitpuller.php
+++ gitpuller.php
@@ -1,5 +1,6 @@
 <?php
 // wird von GitHub als Webhook bei jeden Push zum Repository aufgerufen
+
 if ($_GET["token"] = "PRNuxt8F3X5Kd45qxzTpJBDanYdWXSSccBbXD4NQ") {
   exec("git pull --no-rebase");
 }
