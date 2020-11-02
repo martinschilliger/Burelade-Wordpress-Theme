3757e7283f71e81e5f5527dd4a631e716e642c6d
Martin Schilliger <martin.schilliger@rettung-sg.ch>
2020-11-02 13:05:25 +0100

    gitpuller Konfiguration

--- gitpuller.php
+++ gitpuller.php
@@ -1,7 +1,7 @@
 <?php
 // wird von GitHub als Webhook bei jeden Push zum Repository aufgerufen
 if ($_GET["token"] = "PRNuxt8F3X5Kd45qxzTpJBDanYdWXSSccBbXD4NQ") {
-  exec("git pull");
+  exec("git pull --no-rebase");
 }
 
 ?>
